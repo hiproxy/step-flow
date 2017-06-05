036a20434b048f1bb05b289725231e012ca174cf
zdying <zdying@live.com>
2017-06-05 23:36:35 +0800

    tmp

--- test/steps.js
+++ test/steps.js
@@ -1,4 +1,4 @@
-var Steps = require('../lib/index');
+var Steps = require('../lib/group');
 
 var assert = require('assert');
 
@@ -6,27 +6,26 @@ describe('Steps', function () {
   var seq = new Steps();
 
   seq.use(
-    '/group/',
     function fn1 (ctx, next) {
-      // console.log('fn1:', ctx);
+      console.log('fn1:', ctx);
       ctx.fn1 = true;
       next(null, { time: new Date() });
     },
     function fn11 (ctx, next) {
-      // console.log('fn11:', ctx);
+      console.log('fn11:', ctx);
       ctx.fn11 = true;
       next();
     }
   );
 
   seq.use(function fn2 (ctx, next) {
-    // console.log('fn2:', ctx);
+    console.log('fn2:', ctx);
     ctx.fn2 = true;
     next();
   });
 
-  seq.use('/group/', function fn3 (ctx, next) {
-    // console.log('fn3:', ctx);
+  seq.use(function fn3 (ctx, next) {
+    console.log('fn3:', ctx);
     ctx.fn3 = true;
 
     // next();
@@ -37,6 +36,12 @@ describe('Steps', function () {
     ctx.fn4 = true;
   });
 
+  seq.group('test')
+    .use(function fn5 (ctx, next) {
+      console.log('fn5:', ctx);
+      ctx.fn5 = true;
+    });
+
   describe('# run without filter param', function () {
     var ctx = {
       initialized: true
@@ -45,38 +50,39 @@ describe('Steps', function () {
     seq.run(ctx);
 
     it('# should run default functions', function () {
-      assert.equal(ctx.fn2, true);
-      assert.equal(ctx.fn4, true);
-    });
-
-    it('# should not run group functions', function () {
-      assert.notEqual(ctx.fn1, true);
-      assert.notEqual(ctx.fn11, true);
-      assert.notEqual(ctx.fn3, true);
-    });
-  });
-
-  describe('# run with filter param', function () {
-    var ctx = {
-      initialized: true
-    };
-
-    seq.run('/group/', ctx);
-
-    it('# should run group functions', function () {
+      console.log('==>', ctx);
       assert.equal(ctx.fn1, true);
       assert.equal(ctx.fn11, true);
+      assert.equal(ctx.fn2, true);
       assert.equal(ctx.fn3, true);
     });
 
-    it('# should run every functions passed to `use()`', function () {
-      assert.equal(ctx.fn1, true);
-      assert.equal(ctx.fn11, true);
-    });
-
     it('# should not run group functions', function () {
-      assert.notEqual(ctx.fn2, true);
-      assert.notEqual(ctx.fn4, true);
+      assert.notEqual(ctx.fn5, true);
     });
   });
+
+  // describe('# run with filter param', function () {
+  //   var ctx = {
+  //     initialized: true
+  //   };
+
+  //   seq.run('test', ctx);
+
+  //   it('# should run group functions', function () {
+  //     assert.equal(ctx.fn1, true);
+  //     assert.equal(ctx.fn11, true);
+  //     assert.equal(ctx.fn3, true);
+  //   });
+
+  //   it('# should run every functions passed to `use()`', function () {
+  //     assert.equal(ctx.fn1, true);
+  //     assert.equal(ctx.fn11, true);
+  //   });
+
+  //   it('# should not run group functions', function () {
+  //     assert.notEqual(ctx.fn2, true);
+  //     assert.notEqual(ctx.fn4, true);
+  //   });
+  // });
 });
