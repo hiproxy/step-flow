b99edbb4a6c32d30a4549df5ddd8d8ee005359ef
zdying <zdying@live.com>
2017-06-07 10:53:40 +0800

    add new test case

--- test/other.js
+++ test/other.js
@@ -10,19 +10,42 @@ describe('# next()', function () {
     function fn1 (ctx, next) {
       context = ctx;
       ctx.fn1 = true;
-      next();
+      next(null, 11);
     },
-    function fn11 (ctx, next) {
+    function fn11 (ctx, next, nextTo, data) {
       ctx.fn11 = true;
+      ctx.fn11Data = data;
       next();
     }
   );
 
-  it('# should has default context', function () {
-    flow.run();
+  flow.use(
+    'step2',
+    function fn2 (ctx, next, nextTo, data) {
+      ctx.fn2Data = data;
+      next(null, 21);
+    },
 
+    function fn21 (ctx, next, nextTo, data) {
+      ctx.fn21Data = data;
+      next();
+    }
+  );
+
+  flow.run();
+
+  it('# should has default context', function () {
     assert.notEqual(context, null);
     assert.equal(context.fn1, true);
     assert.equal(context.fn11, true);
+  });
+
+  it('# should pass data to the next function', function () {
+    assert.equal(context.fn11Data, 11);
+    assert.equal(context.fn21Data, 21);
+  });
+
+  it('# should only pass data to the next function', function () {
+    assert.equal(context.fn2Data, undefined);
   });
 });
